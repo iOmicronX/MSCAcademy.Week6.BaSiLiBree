686231540ed3730fdf6981ada83b3110f250d38b
Darius Nicolae Alain Tirla <E-darius.tirla@msc.com>
2023-02-01 11:42:28 +0100

    Add Libro Entity

---
+++ MSCAcademy.Week6.BaSiLibree.Core/Entities/Libro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCAcademy.Week6.BaSiLibree.Core.Entities
+{
+    public class Libro
+    {
+        public Guid Id { get; }
+        public string ISBN { get; }
+        public string Titolo { get; }
+        public string Autore { get; }
+        DateOnly Data { get; set; }
+
+        public Libro(string isbn, string titolo, string autore, DateOnly data)
+        {
+            Id= Guid.NewGuid();
+            ISBN= isbn;
+            Titolo= titolo;
+            Autore= autore;
+            Data = data;
+        }
+
+
+    }
+}

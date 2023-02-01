704d66b78bd1df23b1bd3e1456ab854dccfc4cde
e-darius.tirla <e-darius.tirla@HQAPEW1C999-ACY>
2023-02-01 11:23:31 +0100

    Add project files.

---
+++ MSCAcademy.Week6.BaSiLiBree.UI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MSCAcademy.Week6.BaSiLiBree.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

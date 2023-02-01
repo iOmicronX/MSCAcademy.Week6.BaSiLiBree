cc6abc247e3806d7ec8453b5be06bd78ded67c43
Darius Nicolae Alain Tirla <E-darius.tirla@msc.com>
2023-02-01 11:29:07 +0100

    Add develop branch

---
+++ MSCAcademy.Week6.BaSiLibree.Core/Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCAcademy.Week6.BaSiLibree.Core.Entities
+{
+    internal class Class1
+    {
+    }
+}

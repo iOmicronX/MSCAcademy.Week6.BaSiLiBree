0bee576bf3a4c08e3e0161d2b4eb7150a3e2e38c
Andrea Vacca <E-andrea.vacca@msc.com>
2023-02-01 14:58:22 +0100

    Aggiunta reportistica utenti inattivi

---
+++ MSCAcademy.Week6.BaSiLibree.Core/BusinessLogic/BusinessReportisticaInattivi.cs
@@ -0,0 +1,26 @@
+﻿using MSCAcademy.Week6.BaSiLibree.Core.Entities;
+using MSCAcademy.Week6.BaSiLibree.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MSCAcademy.Week6.BaSiLibree.Core.BusinessLogic
+{
+    public class BusinessReportisticaInattivi : IBusinessReportisticaInattivi
+    {
+        private IRepository<Iscritto> _iscritti;
+
+        public BusinessReportisticaInattivi(IRepository<Iscritto> iscritti)
+        {
+            _iscritti = iscritti;
+        }
+        public List<Iscritto> ElencoInattivi(int n)
+        {
+            var utenti = _iscritti.Fetch();
+            var inattivi = utenti.Where(u => DateTime.Now.Month - u.DataUltimoAccesso.Month > n && DateTime.Now.Year != u.DataUltimoAccesso.Year);
+            return (List<Iscritto>)inattivi;
+        }
+    }
+}
